fff7bb07451722ad0c22211f87f89869865e35f4
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-18 15:46:14 +0100

    fix bonus exercise

--- bonus/main.js
+++ bonus/main.js
@@ -2,26 +2,26 @@
 const imgEl = document.querySelector('img');
 console.log(imgEl);
 
+
 //get button node from the document and save it in a variable
 const btnEl = document.querySelector('button');
 console.log(btnEl);
-console.log(btnEl.innerText);
 
 //add event listener to the button
 btnEl.addEventListener('click', function () {
 
   console.log("Button clicked!");
 
-  //check if the lamp is turned on or turned off
-  if (imgEl.src == "./assets/img/white_lamp.png") {
+  //check if the lamp is turned on
+  if (imgEl.src.includes("white_lamp.png")) {
+    //turn on the lamp
     imgEl.src = "./assets/img/yellow_lamp.png";
+    imgEl.alt = "lamp on";
     btnEl.innerText = "Spegni";
-    console.log(imgEl);
-    console.log(btnEl.innerText);
   } else {
+    //turn off the lamp
     imgEl.src = "./assets/img/white_lamp.png";
+    imgEl.alt = "lamp off";
     btnEl.innerText = "Accendi";
-    console.log(imgEl);
-    console.log(btnEl.innerText);
   }
 });
