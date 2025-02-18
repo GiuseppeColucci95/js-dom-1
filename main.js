9a512ae1d2ee5deed506c07a01e6e41a32b36024
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-18 15:23:39 +0100

    add exercise

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 //get image node from the document and save it in a variable
+const imgEl = document.querySelector('img');
+console.log(imgEl);
 
 //get button node from the document and save it in a variable
+const btnEl = document.querySelector('button');
+console.log(btnEl);
 
 //add event listener to the button
+btnEl.addEventListener('click', function () {
+  imgEl.src = "./assets/img/yellow_lamp.png";
+  imgEl.alt = "lamp on";
+  console.log("Button clicked!");
+  console.log(imgEl);
+});
