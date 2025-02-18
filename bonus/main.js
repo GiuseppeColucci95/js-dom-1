ff2569a88113ee5548f818220982966386a99e1c
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-18 15:35:31 +0100

    add bonus exercise (to fix)

--- bonus/main.js
+++ bonus/main.js
@@ -5,11 +5,23 @@ console.log(imgEl);
 //get button node from the document and save it in a variable
 const btnEl = document.querySelector('button');
 console.log(btnEl);
+console.log(btnEl.innerText);
 
 //add event listener to the button
 btnEl.addEventListener('click', function () {
-  imgEl.src = "./assets/img/yellow_lamp.png";
-  imgEl.alt = "lamp on";
+
   console.log("Button clicked!");
-  console.log(imgEl);
+
+  //check if the lamp is turned on or turned off
+  if (imgEl.src == "./assets/img/white_lamp.png") {
+    imgEl.src = "./assets/img/yellow_lamp.png";
+    btnEl.innerText = "Spegni";
+    console.log(imgEl);
+    console.log(btnEl.innerText);
+  } else {
+    imgEl.src = "./assets/img/white_lamp.png";
+    btnEl.innerText = "Accendi";
+    console.log(imgEl);
+    console.log(btnEl).innerText;
+  }
 });
