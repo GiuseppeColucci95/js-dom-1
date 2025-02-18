e41d043fe56cdc2e71bd63b1ac4dcfe0f91dd430
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-18 15:17:01 +0100

    add html structure and first js human code

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("hi");
+//get image node from the document and save it in a variable
+
+//get button node from the document and save it in a variable
+
+//add event listener to the button
